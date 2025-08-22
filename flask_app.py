5bd9aa3913c243d8c3a2e1d7c60082874832376d
Ana Flávia De Brito Fernandes <pt3022242@ifsproot.ptb>
2025-08-22 19:28:34 -0300

    save resolução

--- flask_app.py
+++ flask_app.py
@@ -1,21 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, request, make_response, redirect, abort;
+app = Flask(__name__);
 
-app = Flask(__name__)
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1><h2>Disciplina PTBDSWS</h2>';
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name);
 
-@app.route("/identificacao")
-def identificacao():
-    return render_template("identificacao.html")
+@app.route('/contextorequisicao')
+def contextorequisicao():
+    user_agent = request.headers.get('User-Agent');
+    return '<p>Your browser is {}</p>'.format(user_agent);
 
-@app.route("/contexto")
-def contexto():
-    return render_template("contexto.html")
+@app.route('/codigostatusdiferente')
+def codigostatusdiferente():
+    return '<p>Bad request</p>', 400;
 
+@app.route('/objetoresposta')
+def objetoresposta():
+    response = make_response('<h1>This document carries a cookie!</h1>');
+    response.set_cookie('answer', '42');
+    return response
 
-# no pythonanywhere, não precisa do app.run()
-# mas localmente você pode rodar:
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/redirecionamento')
+def redirecionamento():
+    return redirect('https://ptb.ifsp.edu.br/');
+
+@app.route('/abortar')
+def abortar():
+    abort(404);
