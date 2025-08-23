c71dffd49fe8210ec9f81551b6aac2eba62c5201
britofernandes <brito.fernandes@aluno.ifsp.edu.br>
2025-08-22 21:35:25 -0300

    22ago

--- flask_app.py
+++ flask_app.py
@@ -1,18 +1,31 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route("/identificacao")
-def identificacao():
-    return render_template("identificacao.html")
+@app.route("/user")
+def user():
+    return render_template("user.html")
 
-@app.route("/contexto")
-def contexto():
-    return render_template("contexto.html")
+@app.errorhandler(404)
+def notfound(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internalerror(e):
+    return render_template('500.html'), 500
+
+# @app.route("/contexto")
+# def contexto():
+#     return render_template("contexto.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
