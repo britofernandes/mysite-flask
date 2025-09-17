da3db1ff2e16c89218627ee469c70171e930654c
britofernandes <brito.fernandes@aluno.ifsp.edu.br>
2025-09-16 23:56:03 -0300

    save 5.1

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,7 @@ from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -23,7 +24,10 @@ def index():
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', 
+                           form=form, 
+                           name=session.get('name'), 
+                           current_time=datetime.utcnow())
 
 @app.route("/user")
 def user():
